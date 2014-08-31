516630943d2d140b95911c79056ea3abeba94232
serge <linserga@gmail.com>
2014-08-31 17:33:52 +0300

    Add saving ribbits

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,20 @@
 class RibbitsController < ApplicationController
+
+	def create
+		@ribbit = current_user.ribbits.new(ribbit_params)
+
+		if @ribbit.save
+			flash[:success] = 'Success'
+		else
+			flash.now[:error] = 'Error'			
+		end
+		redirect_to root_path
+	end
+
+
+	private
+
+		def ribbit_params
+			params.require(:ribbit).permit(:content)
+		end
 end
--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -2,7 +2,7 @@ class Ribbit < ActiveRecord::Base
 
 	belongs_to :user
 
-	default_scope order: 'created_at DESC' 
+	default_scope {order('created_at DESC')}
 
 	validates :content, length:{maximum: 140}
 end
