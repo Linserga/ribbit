5af65aa4719dcb071c9b0cc88b1fd330f983624a
serge <linserga@gmail.com>
2014-08-31 13:41:08 +0300

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  
+  def create
+  	user = User.find_by(:username, params[:username])
+  	if user and user.authenticate(params[:password])
+  		flash[:success] = 'You are logged in'
+  		session[:user_id] = user.id
+  		redirect_t root_path
+  	else
+  		flash[:error] = 'Wrong credentials'
+  		redirect_to root_path
+  	end
+  end
+
+  def destroy
+  	flash[:success] = 'Logged out'
+  	session[:user_id] = nil
+  	redirect_to root_path
+  end
+end
