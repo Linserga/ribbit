d1e29a38674bbbb9b92b15b4b76b7216c40f8f4e
serge <linserga@gmail.com>
2014-08-31 13:33:03 +0300

    Create root_path, users.new, users.show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			flash[:success] = 'Account was successfully created'
+			redirect_to @user
+		else
+			flash.now[:error] = 'Error occured'
+			render :new
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
 	def user_params
 		params.require(:user).permit(:avatar_url, :bio, :email, :name, :password, :password_confirmation, :username)
 	end
